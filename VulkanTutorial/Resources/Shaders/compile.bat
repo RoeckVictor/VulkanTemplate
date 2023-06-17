78d9cf9c8376b90e9f65b330399cc60f594b498c
ROECK VICTOR <victor.roeck@etu.unistra.fr>
2023-06-17 22:42:15 +0200

    Changed gitignore to ignore Indermediate folder, removed useless files

--- VulkanTutorial/Resources/Shaders/compile.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set "SDK_PATH=D:\Files\Autodidacte\Tutorials\Vulkan\VulkanTemplate\VulkanTutorial\Dependencies\VulkanSDK\1.3.243.0\Bin"
-set "SHADER_DIR=%~dp0"
-
-for /r "%SHADER_DIR%" %%f in (*.vert *.frag) do (
-    echo Compiling: %%f
-    "%SDK_PATH%\glslc.exe" "%%f" -o "%%~dpnxf.spv"
-)
-pause
